a1a9c60635028d8f149c279af3621a8cf76f52a9
TOHID KHAN <162583970+Mustak24@users.noreply.github.com>
2024-12-27 12:41:41 +0530

    Add initial implementation of YouTube ad skipping extension

---
+++ script.js
@@ -0,0 +1,29 @@
+const button = document.createElement('button');
+
+button.innerHTML = 'Skip Add';
+button.style = `
+    position: fixed; 
+    bottom: 10px; 
+    left: 10px; 
+    z-index: 9999;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    border-radius: 10px;
+    border: 2px solid white;
+    cursor: pointer;
+`;
+
+button.onclick = (e) => {
+    let video = document.querySelector('video');
+    if(!video) return;
+    video.currentTime = video.duration;
+};
+
+document.body.appendChild(button);
+
+addEventListener('click', () => {
+    let video = document.querySelector('video');
+    if(!video) return;
+    video.currentTime = video.duration;
+})
